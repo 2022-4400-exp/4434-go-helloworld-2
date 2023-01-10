0cabe56447a152598b51ece0cc5c13d377af2048
m19109 <m19109@g.metro-cit.ac.jp>
2023-01-10 15:21:40 +0900

    second commit

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4434", "message": "おーい、お茶"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4434", "message": "チャチャチャの茶"})
     }
 }
